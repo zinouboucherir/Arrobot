7da477f0352288ed938b0a52510258a643738e74
ELMOHRI othmane <o.elmohri@esi-sba.dz>
2022-01-10 09:00:25 +0100

    add REcycle Adapter 3

--- app/src/main/java/com/example/projet_mobile/ListeFrequenceActivity.kt
+++ app/src/main/java/com/example/projet_mobile/ListeFrequenceActivity.kt
@@ -9,6 +9,7 @@ import com.example.projet_mobile.databinding.ActivityListeFrequenceBinding
 import com.example.projet_mobile.databinding.ActivityMainBinding
 
 class ListeFrequenceActivity : AppCompatActivity() {
+    lateinit var adapter: RecycleAdapter3
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         
---
+++ app/src/main/java/com/example/projet_mobile/RecyclerAdapter3.kt
@@ -0,0 +1,39 @@
+package com.example.projet_mobile
+
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.projet_mobile.databinding.ItemLayout3Binding
+import com.example.projet_mobile.databinding.ItemLayoutBinding
+
+class RecycleAdapter3(val context: Context, val frequences: MutableList<Frequence>): RecyclerView.Adapter<RecycleAdapter3.VH>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+        val binding = ItemLayout3Binding
+            .inflate(LayoutInflater.from(parent.getContext()), parent, false)
+        return VH(binding)
+    }
+
+    override fun onBindViewHolder(holder: VH, position: Int) {
+        holder.binding.par.text=frequences[position].NbrFois.toString()
+        holder.binding.chaque.text=frequences[position].Par.toString()
+        holder.binding.moisDebut.text=frequences[position].MoisDebut.toString()
+        holder.binding.moisFin.text=frequences[position].MoisFin.toString()
+
+        holder.binding.modifier.setOnClickListener {
+            val intent = Intent(context, ModifierFrequencecActivity::class.java)
+            intent.putExtra("frequence",frequences[position])
+            context.startActivity(intent)
+        }
+
+    }
+    override fun getItemCount(): Int =frequences.size
+
+    class VH(val binding: ItemLayout3Binding) : RecyclerView.ViewHolder(binding.root)
+
+
+}
