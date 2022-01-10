603d6a18de0a3608c19a3b9c3e62baf3bc3d92f7
ELMOHRI othmane <o.elmohri@esi-sba.dz>
2022-01-10 08:58:23 +0100

    add Liste Frequence activity

---
+++ app/src/main/java/com/example/projet_mobile/ListeFrequenceActivity.kt
@@ -0,0 +1,16 @@
+package com.example.projet_mobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.projet_mobile.databinding.ActivityListeFrequenceBinding
+import com.example.projet_mobile.databinding.ActivityMainBinding
+
+class ListeFrequenceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+    }
+}
